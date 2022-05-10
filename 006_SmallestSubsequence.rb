f9d68efe1bddeccee0c92c347ca91d4973adde48
fumtas1k <fumta01a7@gmail.com>
2022-05-10 11:37:46 +0900

    add:006

---
+++ 006_SmallestSubsequence.rb
@@ -0,0 +1,33 @@
+# 006 - Smallest Subsequence（★5）
+# 貪欲法
+
+N, K = gets.split.map(&:to_i)
+S = gets.chomp.chars
+
+# 前計算（インデックスを記録)
+nex = Array.new(N + 1) { [N] * 26 }
+(N - 1).downto(0) do |i|
+  # Sのindexがiの時点で最も左にある("a".ord + j).chrのSでのindexをメモ
+  26.times do |j|
+    nex[i][j] = nex[i + 1][j]
+  end
+  nex[i][S[i].ord - "a".ord] = i
+end
+
+# 1文字ずつ貪欲に求める
+ans = ""
+# Sのindex番号をposに代入. 0から開始
+pos = 0
+K.times do |i|
+  26.times do |j|
+    # ("a".ord + j).chr のSでのindexをnext_posに代入
+    next_pos = nex[pos][j]
+    # next_posの位置の文字を選択した時、残り文字数が足りているか確認
+    next if N - next_pos < K - i
+    ans << S[next_pos]
+    pos = next_pos + 1
+    break
+  end
+end
+
+puts ans
--- README.md
+++ README.md
@@ -10,3 +10,5 @@ atcoderの典型90問のrubyによる解法集.
 2. [002 - Encyclopedia of Parentheses（★3）](https://atcoder.jp/contests/typical90/tasks/typical90_b) -> [解](002_EncyclopediaOfParentheses.rb)
 3. [003 - Longest Circular Road（★4）](https://atcoder.jp/contests/typical90/tasks/typical90_c) -> [解](003_LongestCircularRoad.rb)
 4. [004 - Cross Sum（★2）](https://atcoder.jp/contests/typical90/tasks/typical90_d) -> [解](004_CrossSum.rb)
+5. 005
+6. [006 - Smallest Subsequence（★5）](https://atcoder.jp/contests/typical90/tasks/typical90_f) -> [解](006_SmallestSubsequence.rb)
