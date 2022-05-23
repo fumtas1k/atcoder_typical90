74c151005b9db6ab9572cd31422dfb67f31e77d1
fumtas1k <fumta01a7@gmail.com>
2022-05-23 17:44:32 +0900

    add:018追加

---
+++ 018_StatueOfChokudai.rb
@@ -0,0 +1,17 @@
+# 018 - Statue of Chokudai（★3）
+# 三角関数
+
+T = gets.to_i
+L, X, Y = gets.split.map(&:to_i)
+Q = gets.to_i
+E = Array.new(Q) { gets.to_i }
+
+angles = E.map do |t|
+  y = - Math.sin(2 * Math::PI * t / T) * L / 2
+  z = (1 - Math.cos(2 * Math::PI * t / T)) * L / 2
+  dist = Math.sqrt(X ** 2 + (Y - y) ** 2)
+  # Math.atan2(y, x): y / x の逆正接関数の値を返す [- π, π]の範囲
+  Math.atan2(z, dist) * 180 / Math::PI
+end
+
+puts angles
