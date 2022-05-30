e015263c50717788e618bbdeb87aef7e41c8fc7d
fumtas1k <fumta01a7@gmail.com>
2022-05-30 11:24:41 +0900

    fix:構文修正

--- 024_Select+／-One.rb
+++ 024_Select+／-One.rb
@@ -6,4 +6,4 @@ AB= Array.new(2) { gets.split.map(&:to_i) }.transpose
 # AiとBiの差の絶対値の合計が一致させるための必要な最小操作回数
 diff = AB.sum { (_1 - _2).abs }
 # Kが最小操作回数以上かつ、Kと最小操作回数のパリティ（偶数/奇数）かの確認
-puts (diff <= K && diff[0] == K[0]) ? "Yes" : "No"
+puts diff <= K && diff[0] == K[0] ? "Yes" : "No"
