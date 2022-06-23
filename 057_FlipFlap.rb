46ec2b77016ba169581c1bff738728cd09aec0f1
fumtas1k <fumta01a7@gmail.com>
2022-06-23 11:19:42 +0900

    fix:構文修正

--- 057_FlipFlap.rb
+++ 057_FlipFlap.rb
@@ -15,26 +15,24 @@ pos = 0
 M.times do |i|
   found = false
   (pos...N).each do |j|
-    if D[j][i] == 1
-      D[pos], D[j] = D[j], D[pos] if pos != j
-      found = true
-      break
+    next if D[j][i] == 0
+    D[pos], D[j] = D[j], D[pos] if pos != j
+    found = true
+    break
+  end
+  next unless found
+  (pos...N).each do |j|
+    next if j == pos || D[j][i] == 0
+    (i...M).each do |k|
+      D[j][k] ^= D[pos][k]
     end
   end
-  if found
-    (pos...N).each do |j|
-      next if j == pos || D[j][i] == 0
-      (i...M).each do |k|
-        D[j][k] ^= D[pos][k]
-      end
+  if S[i] == 1
+    (i...M).each do |j|
+      S[j] ^= D[pos][j]
     end
-    if S[i] == 1
-      (i...M).each do |j|
-        S[j] ^= D[pos][j]
-      end
-    end
-    pos += 1
   end
+  pos += 1
 end
 
 ans = 0
