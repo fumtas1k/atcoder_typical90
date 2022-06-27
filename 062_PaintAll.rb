907b72e264b9fc0dd15cb9e3e951da4188f5c73c
fumtas1k <fumta01a7@gmail.com>
2022-06-27 13:47:10 +0900

    add:062追加

---
+++ 062_PaintAll.rb
@@ -0,0 +1,30 @@
+# 062 - Paint All（★6）
+# 幅優先探索, 後ろから考える
+
+# 後ろから考えるので全てのボールが黒色
+N = gets.to_i
+G = Array.new(N) { [] }
+usable = [false] * N # すでに白にできたもの
+queue = [] # 白に変更したリスト
+
+N.times do |i|
+  a, b = gets.split.map { _1.to_i - 1 }
+  G[a] << i
+  G[b] << i
+  next if a != i && b != i
+  usable[i] = true
+  queue << i
+end
+
+ans = []
+while !queue.empty?
+  pos = queue.shift
+  ans << pos + 1
+  G[pos].each do |i|
+    next if usable[i]
+    usable[i] = true
+    queue << i
+  end
+end
+
+puts ans.size == N ? ans.reverse : -1
