cb54fddad2c4f77ac50e6af09af65f08b1480a87
fumtas1k <fumta01a7@gmail.com>
2022-07-22 00:09:19 +0900

    doc:コメント追加

--- 049_FlipDigits2.rb
+++ 049_FlipDigits2.rb
@@ -25,18 +25,22 @@ class UnionFind
 end
 
 N, M = gets.split.map(&:to_i)
+# クラスカル法を用いるため価格でsortする
 # sortよりもsort_by(&:first)の方が0.2s程速い
 CLR = Array.new(M) { gets.split.map(&:to_i) }.sort_by(&:first)
 
 cost = cnt = 0
+# N+1頂点のグラフが連結できるかを考える
 uf = UnionFind.new(N + 1)
 ans = -1
 
+# Union-Findとクラスカル法で最小全域木を求める
 CLR.each do |c, l, r|
   next if uf.same?(l - 1, r)
   uf.unite(l - 1, r)
   cost += c
   cnt += 1
+  # cnt == Nでグラフが連結
   if cnt == N
     ans = cost
     break
