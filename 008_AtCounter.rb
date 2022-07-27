d6347aea1e367f5863a99008836b3de42cb4ac71
fumtas1k <fumta01a7@gmail.com>
2022-07-27 11:13:43 +0900

    fix:コメント間違い修正

--- 008_AtCounter.rb
+++ 008_AtCounter.rb
@@ -1,20 +1,22 @@
 # 008 - AtCounter（★4）
 # 状態DP法
 
-N = gets.to_i
-S = gets.chomp
-T = "atcoder"
 MOD = 10 ** 9 + 7
 
+N = gets.to_i
+S = gets.chomp.chars
+T = "atcoder".chars
+
 # dp[Sの文字の位置(1~N)][Tの文字の位置(1~T.size)]
-# dp[i][0] = 1 なのは、""を表す方法が1通りのみだから(どれも使用しない)
-dp = Array.new(N + 1){[1] + [0] * T.size }
+dp = Array.new(N + 1) { [0] * (T.size + 1) }
+# dp[0][0] = 1 なのは、""を表す方法が1通りのみだから(どれも使用しない)
+dp[0][0] = 1
 
 N.times do |i|
-  T.size.times do |j|
-    dp[i + 1][j + 1] += dp[i][j + 1] # T[j]を使わないパターン
-    dp[i + 1][j + 1] += dp[i][j] if S[i] == T[j] # T[j]を使うパターン
-    dp[i + 1][j + 1] %= MOD
+  (T.size + 1).times do |j|
+    dp[i + 1][j] += dp[i][j] # S[i]を使わないパターン
+    dp[i + 1][j + 1] += dp[i][j] if j < N && S[i] == T[j] # S[i]を使うパターン
+    dp[i + 1][j] %= MOD
   end
 end
 
