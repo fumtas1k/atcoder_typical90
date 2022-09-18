c4adaa85718a6519a80646fd229674bd7c420f8d
fumtas1k <fumta01a7@gmail.com>
2022-09-18 23:27:34 +0900

    [improve]シンプルに

--- ruby/060_Chimera.rb
+++ ruby/060_Chimera.rb
@@ -4,15 +4,15 @@
 N = gets.to_i
 A = gets.split.map(&:to_i)
 
-# 返す配列のi番目はarr[0..i - 1]にarr[i]を加えた時に更新されるLISの長さ
+# 返す配列のi番目はi番目の値を最大とするLISの長さ
 # dpは短調増加だが、返す配列は短調増加ではない。
 # arr自体の最長増加部分列の長さはdpのsize or 返り値の最大値で求まる
 def lis(arr)
   n = arr.size
   dp = []
-  n.times.reduce([]) do |result, i|
-    pos = dp.bsearch_index { _1 >= arr[i] } || dp.size
-    dp[pos] = arr[i]
+  arr.reduce([]) do |result, a|
+    pos = dp.bsearch_index { _1 >= a } || dp.size
+    dp[pos] = a
     result << pos + 1
   end
 end
