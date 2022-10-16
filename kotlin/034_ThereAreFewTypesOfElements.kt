aef37a66e8da34be9acba26095acaa55f397f87f
fumtas1k <fumta01a7@gmail.com>
2022-10-16 22:13:55 +0900

    [add]034追加修正

---
+++ kotlin/034_ThereAreFewTypesOfElements.kt
@@ -0,0 +1,30 @@
+/** 034 - There are few types of elements（★4）
+ * 尺取法
+ */
+
+fun main() {
+
+  val (N, K) = readLine()!!.split(" ").map(String::toInt)
+  val A = readLine()!!.split(" ").map(String::toInt)
+
+  var ans = 0
+  var cnt = 0
+  var useCnt = mutableMapOf<Int, Int>().withDefault { 0 }
+  var right = 0
+  for (left in 0 until N) {
+    while (right < N) {
+      val a = A[right]
+      if (useCnt.getValue(a) == 0) {
+        if (cnt == K) break
+        cnt++
+      }
+      useCnt.put(a, useCnt.getValue(a) + 1)
+      right++
+    }
+    ans = Math.max(ans, right - left)
+    useCnt.put(A[left], useCnt.getValue(A[left]) - 1)
+    if (useCnt[A[left]] == 0) cnt--
+  }
+
+  println(ans)
+}
--- ruby/034_ThereAreFewTypesOfElements.rb
+++ ruby/034_ThereAreFewTypesOfElements.rb
@@ -4,21 +4,23 @@
 N, K = gets.split.map(&:to_i)
 A = gets.split.map(&:to_i)
 
-memo = Hash.new(0)
+use_cnt = Hash.new(0)
 ans = cnt = right = 0
 
 N.times do |left|
   while right < N
     a = A[right]
-    break if memo[a] == 0 && cnt == K
-    cnt += 1 if memo[a] == 0
-    memo[a] += 1
+    if use_cnt[a] == 0
+      break if cnt == K
+      cnt += 1
+    end
+
+    use_cnt[a] += 1
     right += 1
   end
   ans = [ans, right - left].max
-  cnt -= 1 if memo[A[left]] == 1
-  memo[A[left]] -= 1
-  right = [right, left + 1].max
+  use_cnt[A[left]] -= 1
+  cnt -= 1 if use_cnt[A[left]] == 0
 end
 
 puts ans
