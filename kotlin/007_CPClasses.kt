b5894d6f23afcab2b9b842d628d99acd813011e4
fumtas1k <fumta01a7@gmail.com>
2023-08-06 21:45:01 +0900

    [improve]二分探索改良

--- kotlin/007_CPClasses.kt
+++ kotlin/007_CPClasses.kt
@@ -9,14 +9,23 @@ fun main() {
   val Q = readLine()!!.toInt()
   val B = List(Q) { readLine()!!.toInt() }
 
-  val ans = mutableListOf<Int>()
-  for (b in B) {
-    val idx = A.binarySearch(b).let { if (it >= 0) it else it.inv() }
-    var min = 1000_000_000
-    if (idx < N) min = Math.min(min, Math.abs(A[idx] - b))
-    if (idx > 0) min = Math.min(min, Math.abs(A[idx - 1] - b))
-    ans.add(min)
+  B.forEach {
+    val idx = A.lowerBound(it)
+    var discontent = 1_000_000_000
+    if (idx > 0) discontent = Math.min(discontent, Math.abs(it - A[idx - 1]))
+    if (idx < N) discontent = Math.min(discontent, Math.abs(it - A[idx]))
+    println(discontent)
   }
+}
 
-  ans.forEach(::println)
+/**
+ * LowerBound
+ *
+ * @param element 検索したい値
+ * @param fromIndex 開始位置
+ * @param toIndex 終了位置(この位置は含まない)
+ * @return element以上の値が出現する一番左のインデックス
+ */
+fun <T: Comparable<T>> List<T>.lowerBound(element: T, fromIndex: Int = 0, toIndex: Int = size): Int {
+  return binarySearch(element, Comparator {a, b -> if (a.compareTo(b) >= 0) 1 else -1 }, fromIndex, toIndex).inv()
 }
