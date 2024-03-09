3b049aea278af703ce416c2d6acd2b2fe8d3933c
fumtas1k <fumta01a7@gmail.com>
2024-03-09 14:13:28 +0900

    [improve]コメント修正

--- ruby/037_DontLeaveTheSpice.rb
+++ ruby/037_DontLeaveTheSpice.rb
@@ -10,26 +10,22 @@ dp[0][0] = 0
 LRV.each_with_index do |(l, r, v), i|
   # スライド最大値を使用して区間の最大値を求める
   max_idxs = []
-  first = true
   (W + 1).times do |j|
     dp[i + 1][j] = dp[i][j]
     next if j < l
     cl = [0, j - r].max
     cr = [0, j - l].max
 
-    if first
-      # max_idxsの左端がdp[i]のcl~cr区間最大値
-      max_idxs = [*cl..cr].sort_by { - dp[i][_1] }
-      first = false
-    end
+    # max_idxsの左端がdp[i]のcl~cr区間最大値
+    max_idxs = [*cl..cr].sort_by { - dp[i][_1] } if max_idxs.empty?
 
-    # crがmax_idxsの右端(最小)になるよう設定
-    max_idxs.pop while !max_idxs.empty? && dp[i][max_idxs[-1]] <= dp[i][cr]
-
-    # clよりindexが小さいものは候補とならないので取り除く
+    # clよりindexが小さいものは最大値候補とならないので取り除く
     max_idxs.shift while !max_idxs.empty? && max_idxs[0] < cl
 
+    # dp[cr]以下の価値は今後最大値候補とならないのでそのindexを取り除く
+    max_idxs.pop while !max_idxs.empty? && dp[i][max_idxs[-1]] <= dp[i][cr]
     max_idxs << cr
+
     val = dp[i][max_idxs[0]]
     next if val == -1
     dp[i + 1][j] = [dp[i + 1][j], val + v].max
