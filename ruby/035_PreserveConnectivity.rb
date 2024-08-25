a2c222ced6a97fa1de18c33137baf13687f990bc
fumtas1k <fumta01a7@gmail.com>
2024-08-25 21:11:46 +0900

    [improve]わかりやすい変数名に修正

--- ruby/035_PreserveConnectivity.rb
+++ ruby/035_PreserveConnectivity.rb
@@ -2,75 +2,73 @@
 # 木DP, 最近共通祖先(LCA), 座標圧縮
 
 N = gets.to_i
-G = Array.new(N) { [] }
+BIT = N.bit_length
+G = Array.new(N){[]}
 (N - 1).times do
-  a, b = gets.split.map { _1.to_i - 1 }
+  a, b = gets.split.map(&:to_i).map(&:pred)
   G[a] << b
   G[b] << a
 end
+Q = gets.to_i
 
-BITS = N.bit_length
-# ダブリング用
-@par = Array.new(BITS) { [0] * N }
+@parents = Array.new(BIT) { [0] * N }
 # 頂点0からの深さ
 @depth = [0] * N
 # dfs順の番号をメモ
-@id = [0] * N
-@k_id = 0
+@orders = []
+# 現在の順番
+@cur_ord = 0
 
-def build_tree(pos, pre)
-  @par[0][pos] = pre
-  @id[pos] = @k_id
-  @k_id += 1
-
-  G[pos].each do |i|
-    next if i == pre
-    @depth[i] = @depth[pos] + 1
-    build_tree(i, pos)
+def dfs(pos = 0, pre = 0)
+  @parents[0][pos] = pre
+  @orders[pos] = @cur_ord
+  @cur_ord += 1
+  G[pos].each do |to|
+    next if to == pre
+    @depth[to] = @depth[pos] + 1
+    dfs(to, pos)
   end
 end
 
-build_tree(0, 0)
+dfs
 
 # ダブリング
-(1...BITS).each do |i|
+(BIT - 1).times do |i|
   N.times do |j|
-    @par[i][j] = @par[i - 1][@par[i - 1][j]]
+    @parents[i + 1][j] = @parents[i][@parents[i][j]]
   end
 end
 
 # LCA(最近共通祖先)
-def lca(va, vb)
-  va, vb = vb, va if @depth[va] < @depth[vb]
-  (BITS - 1).downto(0) do |i|
-    next if @depth[va] - @depth[vb] < (1 << i)
-    va = @par[i][va]
+def lca(u, v)
+  u, v = v, u if @depth[u] < @depth[v]
+  diff = @depth[u] - @depth[v]
+  BIT.times do |i|
+    next if diff[i].zero?
+    u = @parents[i][u]
   end
 
-  return va if va == vb
-  (BITS - 1).downto(0) do |i|
+  return u if u == v
+
+  (BIT - 1).downto(0) do |i|
     # 2のi乗個上の頂点ではなく1個上の頂点が知るため
-    next if @par[i][va] == @par[i][vb]
-    va = @par[i][va]
-    vb = @par[i][vb]
+    next if @parents[i][u] == @parents[i][v]
+    u = @parents[i][u]
+    v = @parents[i][v]
   end
-
-  @par[0][va]
+  @parents[0][u]
 end
 
-Q = gets.to_i
 ans = [0] * Q
 Q.times do |i|
-  kv = gets.split.map(&:to_i)
-  k = kv.shift
+  k, *v = gets.split.map(&:to_i)
   # 巡回順に並び替え
-  v = kv.map(&:pred).sort_by { @id[_1] }
+  v.map!(&:pred).sort_by! { @orders[_1] }
 
-  k.times do |j|
+  v.each_with_index do |vj, j|
     # v[j]までの深さ から v[(j + 1) % k]との
     # 最近共通祖先までの深さを引くと辺の数が求められる
-    ans[i] += @depth[v[j]]
-    ans[i] -= @depth[lca(v[j], v[(j + 1) % k])]
+    ans[i] += @depth[vj] - @depth[lca(vj, v[(j + 1) % k])]
   end
 end
 
