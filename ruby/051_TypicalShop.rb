fce05254b3346d1e7158cf018775149890a52ce9
fumtas1k <fumta01a7@gmail.com>
2026-02-23 13:07:35 +0900

    [refactor] enumerateメソッドを追加し、価格計算のロジックを整理

--- ruby/051_TypicalShop.rb
+++ ruby/051_TypicalShop.rb
@@ -1,33 +1,37 @@
 # 051 - Typical Shop（★5）
 # 半分全列挙
 
-N, K, P = gets.split.map(&:to_i)
-A = gets.split.map(&:to_i)
-mid = N / 2
-
-# ans[i]はarrからi個選択した時の値段のありうる合計の配列
-def get_kp(arr)
+def enumerate(arr)
   n = arr.size
-  ans = Array.new(n + 1) { [] }
-  ans[0] << 0
-  (1..n).each do |i|
-    arr.combination(i) do |comb|
-      ans[i] << comb.sum
+  hash = Hash.new {|h, k| h[k] = [] }
+  (1 << n).times do |bit|
+    cnt = 0
+    sum = 0
+    n.times do |i|
+      next if bit[i].zero?
+      cnt += 1
+      sum += arr[i]
     end
-    ans[i].sort!
+    hash[cnt] << sum
   end
-  ans
+  hash.each_value(&:sort!)
+  hash
 end
 
-prices1 = get_kp(A[0, mid])
-prices2 = get_kp(A[mid..])
+N, K, P = gets.split.map(&:to_i)
+A = gets.split.map(&:to_i)
+
+mid = N / 2
+left = enumerate(A[0, mid])
+right = enumerate(A[mid, N - mid])
 
 ans = 0
-[mid + 1, K + 1].min.times do |k1|
-  k2 = K - k1
-  next unless k2.between?(0, N - mid)
-  prices1[k1].each do |p1|
-    ans += prices2[k2].bsearch_index { _1 + p1 > P } || prices2[k2].size
+left.each do |k1, l_sum|
+  next if k1 > K
+  l_sum.each do |v|
+    next if v > P
+    r_sum = right[K - k1]
+    ans += r_sum.bsearch_index { _1 + v > P } || r_sum.size
   end
 end
 
